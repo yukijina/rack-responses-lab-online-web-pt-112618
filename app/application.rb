67e8c61281466cb4e522488c50ee1b08941ba517
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-25 19:57:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     current_time = Time.now.utc
     
-    if  current_time < 12
+    if  current_time.hour < 12
       resp.write "Good Morning!\n"
     elsif current_time > 12
       resp.write "Good Afternoon!\n"
