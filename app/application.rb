3115fec2875e46675509e52eec6419337853bbb3
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-25 19:59:30 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application 
+  def call(env)
+    resp = Rack::Response.new
+    current_time = Time.now.utc
+    
+    if  current_time.hour < 12
+      resp.write "Good Morning!\n"
+    elsif current_time.hour > 12
+      resp.write "Good Afternoon!\n"
+    end 
+    resp.finish
+  end 
+end 
